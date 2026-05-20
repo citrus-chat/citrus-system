4f55efb19a82a6f5202939c1dedcfcf6819ee2cd
Zarsu <unusuariodegogle.xd@gmail.com>
2026-05-20 19:38:15 -0300

    spotless again

--- src/main/java/com/javaee2026/citruschat/messaging/domain/exceptions/InvalidChatRoleException.java
+++ src/main/java/com/javaee2026/citruschat/messaging/domain/exceptions/InvalidChatRoleException.java
@@ -4,7 +4,7 @@ import com.javaee2026.citruschat.shared.application.exceptions.BusinessException
 import com.javaee2026.citruschat.shared.domain.errors.ErrorCode;
 
 public class InvalidChatRoleException extends BusinessException {
-    public InvalidChatRoleException(String message) {
-        super(ErrorCode.INVALID_CHATROLE, message);
-    }
+	public InvalidChatRoleException(String message) {
+		super(ErrorCode.INVALID_CHATROLE, message);
+	}
 }
