83dc998f31a2213d7a589b71eca6af1258d5a064
Zarsu <unusuariodegogle.xd@gmail.com>
2026-07-01 11:41:29 -0300

    syncConvo

--- src/main/java/com/javaee2026/citruschat/messaging/infrastructure/websocket/ports/IMessageRealtimeNotifier.java
+++ src/main/java/com/javaee2026/citruschat/messaging/infrastructure/websocket/ports/IMessageRealtimeNotifier.java
@@ -3,6 +3,5 @@ package com.javaee2026.citruschat.messaging.infrastructure.websocket.ports;
 import com.javaee2026.citruschat.shared.domain.valueobjects.ChatRoomId;
 
 public interface IMessageRealtimeNotifier {
-
 	void notifyMessageCreated(ChatRoomId chatRoomId);
 }
